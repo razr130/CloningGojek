b3570911ea3b2d8a02757f8e62edfdb623e221ac
SpaceColony <jovfernando@yahoo.com>
2019-09-25 09:36:00 +0700

    commitan

--- App.js
+++ App.js
@@ -1,114 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class AwesomeApp extends Component {
+  render() {
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    return (
+      <View style={styles.parent}>
+        <View style={{backgroundColor: '#f1c40f', width: '100%', height: 100, borderRadius: 10, flexDirection: "row", padding: 5, marginBottom: 10,  position: 'relative'}}>
+          <View style={{backgroundColor: 'red', width: 25, height: 25, borderRadius: 100, justifyContent: 'center', position: 'absolute', right: -10, top: -10}}>
+            <Text style={{color: 'white', fontWeight: 'bold', textAlign: 'center'}}>1</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </View>
+        <View style={{backgroundColor: '#3498db', flex: 1, height: 80, marginRight: 5, borderRadius: 10, justifyContent: 'center'}}>
+          <Text style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold', textAlign: 'center'}}>HUBA</Text>
+        </View>
+        <View style={[{backgroundColor: '#2ecc71', marginHorizontal: 5}, styles.box]}></View>
+        <View style={[{backgroundColor: '#3498db', marginLeft: 5}, styles.box]}></View>
+        
+    
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  parent: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    flexWrap: 'wrap'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
-export default App;
+  box: {
+    flex: 1,
+    height: 80,
+    borderRadius: 10
+  }
+
+})
