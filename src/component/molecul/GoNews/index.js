cb2c4488c1bf7f4448297b6a5b82c22a3a99318b
SpaceColony <jovfernando@yahoo.com>
2019-09-27 14:44:33 +0700

    can read the fucking json

--- src/component/molecul/GoNews/index.js
+++ src/component/molecul/GoNews/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 
 
-const GoNews = ({onPress}) => {
+const GoNews = (props,{onPress}) => {
     return (
         <View style={{ marginHorizontal: 20, marginVertical: 20, borderBottomColor: '#eeeeee', borderBottomWidth: 1, paddingBottom: 20 }}>
 
             <View>
-                <Image style={{ height: 200, borderRadius: 10 }} source={{ uri: 'https://i.ytimg.com/vi/gsUWM6e6l_c/maxresdefault.jpg' }} />
+                <Image style={{ height: 200, borderRadius: 10 }} source={{ uri: props.newsimage }} />
             </View>
             <View>
-                <Text style={{ fontWeight: 'bold', marginTop: 10, fontSize: 15, color: '#1a1a1a' }}>Ash Win Alolan League</Text>
+                <Text style={{ fontWeight: 'bold', marginTop: 10, fontSize: 15, color: '#1a1a1a' }}>{props.newstitle}</Text>
                 <Text style={{ color: '#7a7a7a', marginTop: 4, fontSize: 12, fontWeight: '500' }}>After 20 years struggling, now finally.</Text>
                 <TouchableOpacity onPress={onPress} style={{
                     backgroundColor: '#61a756',
--- src/container/page/Home/index.js
+++ src/container/page/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, FlatList, Text } from 'react-native';
 import SearchBar from '../../../component/molecul/SearchBar';
 import GoNews from '../../../component/molecul/GoNews';
 import NearbyGoFood from '../../../container/organism/NearbyGoFood';
@@ -7,7 +7,53 @@ import BottomNavBar from '../../../container/organism/BottomNav';
 import GoPay from '../../../container/organism/GoPay';
 import MainFeatureOrganism from '../../../container/organism/MainFeature';
 
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+
 class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            dataSource: []
+        }
+    }
+
+    renderItem = ({item}) => {
+        return(
+            <GoNews newstitle={item.title} newsimage={item.thumbnail_image_name} onPress={() => this.props.navigation.navigate('NewsDetail')} />
+        )
+    }
+
+    componentDidMount(){
+        return fetch('https://s3-us-west-2.amazonaws.com/youtubeassets/home.json')
+          .then((response) => response.json())
+          .then((responseJson) => {
+    
+            this.setState({
+              dataSource: responseJson,
+            }, function(){
+    
+            });
+    
+          })
+          .catch((error) =>{
+            console.error(error);
+          });
+      }
+
     render() {
         return (
             <View style={{
@@ -19,7 +65,12 @@ class Home extends Component {
                     <GoPay />
                     <MainFeatureOrganism />
                     <View style={{ backgroundColor: '#eeeeee', height: 10, marginTop: 20 }}></View>
-                    <GoNews onPress={() => this.props.navigation.navigate('NewsDetail')} />
+                    {/* <GoNews newstitle="Fucking ash" onPress={() => this.props.navigation.navigate('NewsDetail')} /> */}
+                    <FlatList
+                         data={this.state.dataSource}
+                         renderItem={this.renderItem}
+                         keyExtractor={item => item.title}
+                    />
                     <NearbyGoFood />
                 </ScrollView>
                 <BottomNavBar />
